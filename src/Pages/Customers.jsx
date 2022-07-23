fa56d8e7a47270a498f946bc6136310143cfa869
Dinesh Karde <dineshkarde103@gmail.com>
2022-07-23 05:52:43 +0530

    day-4

---
+++ src/Pages/Customers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomerPart1 from '../Components/Customers/CustomerPart1'
+
+const Customers = () => {
+  return (
+    <>
+        <CustomerPart1 />
+    </>
+  )
+}
+
+export default Customers
